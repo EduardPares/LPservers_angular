1a78adfd716aefe7628a54957fb762442edab95d
EduardPares <81320763+EduardPares@users.noreply.github.com>
2022-05-24 17:55:15 +0200

    token

--- lpservers/src/app/dades.service.ts
+++ lpservers/src/app/dades.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
 
 export class DadesService {
+  token:any=""
+  
   urlServidor = "https://xtkyneixvg.execute-api.eu-west-1.amazonaws.com"
   urlServidorE = "http://ec2-3-250-125-124.eu-west-1.compute.amazonaws.com:3000"
   constructor(private http: HttpClient) { }
  
   llegirpacients() {
+    this.token = localStorage.getItem("token")
+    const headers = { 'Authorization': `Bearer ${this.token}` }
     return this.http.get<any>(
-      this.urlServidor + '/pacients/42'
+      this.urlServidor + '/pacients/42', {headers}
     )
   }
 
@@ -23,8 +26,11 @@ export class DadesService {
     )
   }
   llegirdiagnostic(idpacient: any) {
+    this.token = localStorage.getItem("token")
+    
+    const headers = { 'Authorization': `Bearer ${this.token}` }
     return this.http.get<any>(
-      this.urlServidor + '/diagnostics/' + idpacient
+      this.urlServidor + '/diagnostics/' + idpacient, {headers}
     )
   }
   botopacient(idpacientb: any) {
--- lpservers/src/app/login/login.component.ts
+++ lpservers/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DadesService } from '../dades.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public servei:DadesService, public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // this.servei.token = this.route.snapshot.paramMap.get("token");
+    // console.log(this.servei.token)
+    localStorage.clear()
+    localStorage.setItem("token", (window.location.href).split('=')[1].split('&')[0]) 
+
+    if(!localStorage.getItem("token")){
+      alert("no estas loguejat!")
+    }
+    
+    
   }
 
 }
